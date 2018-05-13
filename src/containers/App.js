2f632bee9369d1f7a83e5a488f5071a162208355
Katherine Ebel <kathyebel@me.com>
2018-05-13 08:00:24 -0400

    Create and style navbar component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux'
+import { configureStore } from '../store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './Navbar'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const store = configureStore();
+
+const App = () => (
+  <Provider store={ store }>
+    <Router>
+      <div className="onboarding">
+        <Navbar/>
       </div>
-    );
-  }
-}
+    </Router>
+  </Provider>
+);
+
 
 export default App;
